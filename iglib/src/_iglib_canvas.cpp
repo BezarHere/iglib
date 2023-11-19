bc13534654102045db157b46c9ebaf4937034b2d
botatodev <botatodev@gmail.com>
2023-11-19 23:02:14 +0300

    remove 'to_clamped_space' functions

--- iglib/src/_iglib_canvas.cpp
+++ iglib/src/_iglib_canvas.cpp
@@ -5,19 +5,6 @@
 #include "intrinsics.h"
 #include "draw_internal.h"
 
-FORCEINLINE [[nodiscard]] Vector2f to_clamped_space(const Vector2f &val, const Vector2f wf)
-{
-	(void)wf;
-	return val;
-	//return Vector2f((val.x * 2.0f / wf.x) - 1.0f, -((val.y * 2.0f / wf.y) - 1.0f));
-}
-
-FORCEINLINE [[nodiscard]] Vector2f to_clamped_size(const Vector2f &val, const Vector2f wf)
-{
-	(void)wf;
-	return val;
-	//return Vector2f((val.x * 2.0f / wf.x), -((val.y * 2.0f / wf.y)));
-}
 
 
 typedef basic_heap_span<Vector2f> Vector2fSpan_t;
@@ -52,113 +39,6 @@ FORCEINLINE const Vector2fSpan_t &get_circle_frame(uint16_t res)
 		s_CircleFrames[res] = _generate_circle_frame(res);
 	return s_CircleFrames.at(res);
 }
-
-union vector2_qbuffer
-{
-	static constexpr size_t size = 4;
-	static constexpr size_t fsize = size * 2;
-	static constexpr size_t bits_count = size * sizeof(Vector2f) * 8;
-	static_assert(bits_count == 256, "");
-	
-	vector2_qbuffer(const Vector2f &v0, const Vector2f &v1, const Vector2f &v2, const Vector2f &v3)
-		: v{ v0, v1, v2, v3 }
-	{
-	}
-
-	vector2_qbuffer()
-		: v{}
-	{}
-
-	Vector2f v[ size ];
-	float_t fv[ size * 2 ];
-	float_t f;
-};
-static_assert(sizeof(vector2_qbuffer) == sizeof(__m256), "");
-
-//__forceinline void to_clamped_space(Vector2f &val, const Window &wnd)
-//{
-//  const Vector2f wf{ wnd.get_size() };
-//  val.x = (val.x * 2.0f / wf.x) - 1.0f;
-//  val.y = -((val.y * 2.0f / wf.y) - 1.0f);
-//}
-
-//__forceinline void to_clamped_size(Vector2f &val, const Window &wnd)
-//{
-//  const Vector2f wf{ wnd.get_size() };
-//  val.x = (val.x * 2.0f / wf.x);
-//  val.y = -((val.y * 2.0f / wf.y));
-//}
-
-// Packed __m256 are structured as:
-//    x0|x1|x2|x3|y0|y1|y2|y3
-__forceinline __m256 pack_vector2_q(const float_t *const fv)
-{
-#if 0
-	const float_t *src = &qbuffer.f;
-	float dst[ vector2_qbuffer::fsize ];
-	for (size_t i{}; i < vector2_qbuffer::fsize; i++)
-	{
-		if (i & 0x1) // y-axis (odd)
-			dst[ (i >> 1) + 4 ] = src[ i ];
-		else // x-axis (even)
-			dst[ i >> 1 ] = src[ i ];
-	}
-	return _mm256_load_ps(dst);
-#endif
-	return _mm256_set_ps(fv[ 0 ], fv[ 2 ], fv[ 4 ], fv[ 6 ],
-											 fv[ 1 ], fv[ 3 ], fv[ 5 ], fv[ 7 ]);
-}
-
-// Packed __m256 are structured as:
-//    x0|x1|x2|x3|y0|y1|y2|y3
-__forceinline void unpack_vector2_q(const __m256 mm256, float_t *const fv)
-{
-	fv[ 0 ] = mm256.m256_f32[ 0 ];
-	fv[ 2 ] = mm256.m256_f32[ 2 ];
-	fv[ 4 ] = mm256.m256_f32[ 4 ];
-	fv[ 6 ] = mm256.m256_f32[ 6 ];
-	fv[ 1 ] = mm256.m256_f32[ 1 ];
-	fv[ 3 ] = mm256.m256_f32[ 3 ];
-	fv[ 5 ] = mm256.m256_f32[ 5 ];
-	fv[ 7 ] = mm256.m256_f32[ 7 ];
-}
-
-__forceinline [[nodiscard]] __m256 to_clamped_space(__m256 val, __m256 wnd_size)
-{
-	const __m256 original = val;
-
-	// val *= 2.0f
-	val = _mm256_mul_ps(val, two256);
-
-	// val /= wnd_size
-	val = _mm256_div_ps(val, wnd_size);
-
-	// val -= 1.0f
-	val = _mm256_sub_ps(val, one256);
-
-	// val.y = -val.y
-	{
-		__m128 *ptr128_y = ((__m128 *) & val) + 1;
-		// val_y = val.y
-		__m128 val_y = *ptr128_y;
-
-		// val_y = 0 - val_y # negation
-		val_y = _mm_sub_ps(zero128, val_y);
-
-		// val.y = val_y
-		*ptr128_y = val_y;
-	}
-	return val;
-}
-
-__forceinline [[nodiscard]] void to_clamped_space(vector2_qbuffer &val, const Vector2f wf)
-{
-	// idk why flipping the x and y fixes some stretch proplems
-	const __m256 wf256 = _mm256_set_ps(wf.y, wf.y, wf.y, wf.y, wf.x, wf.x, wf.x, wf.x);
-	unpack_vector2_q(to_clamped_space(pack_vector2_q(val.fv), wf256), val.fv);
-}
-
-
 static ShaderInstance_t DefaultShader2D{};
 static ShaderInstance_t DefaultShader3D{};
 
