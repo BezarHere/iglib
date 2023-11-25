6badfc91c16a19bdc8147fcd8a9fcff18660553c
botatodev <botatodev@gmail.com>
2023-11-25 22:41:18 +0300

    no more made real vector2s more streamlined

--- iglib/src/_iglib_vector.h
+++ iglib/src/_iglib_vector.h
@@ -111,6 +111,22 @@ namespace ig
 			return this_type(x / l, y / l);
 		}
 
+		inline _T angle() const
+		{
+			return std::atan2( this->y, this->x );
+		}
+
+		inline _T angle_to( const this_type &other ) const
+		{
+			return std::atan2( other.y - this->y, other.x - this->x );
+		}
+
+		inline this_type rotated( const _T radians ) const
+		{
+			const _T sin = std::sin( radians ), cos = std::cos( radians );
+			return this_type( (this->y * sin) + (this->x * cos), (this->x * sin) + (this->y * cos) );
+		}
+
 		inline constexpr this_type operator+(const this_type &other) const
 		{
 			return this_type(x + other.x, y + other.y);
@@ -139,12 +155,6 @@ namespace ig
 		inline constexpr this_type operator/(const _T other) const
 		{
 			return this_type(x / other, y / other);
-		}
-
-		template <typename _E>
-		inline constexpr this_type operator&(const _E bits) const
-		{
-			return this_type(x >> bits, y >> bits);
 		}
 
 		inline this_type &operator+=(const this_type &other)
@@ -207,49 +217,6 @@ namespace ig
 		_T x, y;
 	};
 
-	template <typename _T>
-	struct RealVector2Template : public BaseVector2Template<_T>
-	{
-		using base_type = BaseVector2Template<_T>;
-		using this_type = RealVector2Template<_T>;
-		using value_type = _T;
-
-
-		constexpr inline RealVector2Template(_T x_, _T y_)
-			: base_type(x_, y_)
-		{
-		}
-
-
-		constexpr inline RealVector2Template()
-			: base_type()
-		{
-		}
-
-		template <typename _E>
-		constexpr inline RealVector2Template(const BaseVector2Template<_E> &copy)
-			: base_type(copy)
-		{
-		}
-
-		inline _T angle() const
-		{
-			return std::atan2(this->y, this->x);
-		}
-
-		inline _T angle_to(const this_type &other) const
-		{
-			return std::atan2(other.y - this->y, other.x - this->x);
-		}
-
-		inline this_type rotated(const _T radians) const
-		{
-			const _T sin = std::sin(radians), cos = std::cos(radians);
-			return this_type((this->y * sin) + (this->x * cos), (this->x * sin) + (this->y * cos));
-		}
-
-	};
-
 	template <typename _T, typename _R>
 	struct IntegralVector2Template : public BaseVector2Template<_T>
 	{
@@ -257,7 +224,7 @@ namespace ig
 		using this_type = IntegralVector2Template<_T, _R>;
 		using real_type = _R;
 		using integral_type = _T;
-		using real_this_type = RealVector2Template<_R>;
+		using real_this_type = BaseVector2Template<_R>;
 
 
 		constexpr inline IntegralVector2Template(_T x_, _T y_)
@@ -306,6 +273,12 @@ namespace ig
 		}
 
 		template <typename _E>
+		inline constexpr this_type operator&( const _E bits ) const
+		{
+			return this_type( this->x >> bits, this->y >> bits );
+		}
+
+		template <typename _E>
 		inline constexpr this_type operator<<(const _E bits) const
 		{
 			return this_type(this->x << bits, this->y << bits);
@@ -327,12 +300,6 @@ namespace ig
 		inline constexpr this_type operator>>(const BaseVector2Template<_E> bits_v) const
 		{
 			return this_type(this->x >> bits_v.x, this->y >> bits_v.y);
-		}
-
-		template <typename _E>
-		inline constexpr this_type operator&(const _E bits) const
-		{
-			return this_type(this->x & bits, this->y & bits);
 		}
 
 		template <typename _E>
@@ -805,8 +772,8 @@ namespace ig
 		value_type x, y, z, w;
 	};
 
-	using Vector2f = RealVector2Template<float_t>;
-	using Vector2d = RealVector2Template<double_t>;
+	using Vector2f = BaseVector2Template<float_t>;
+	using Vector2d = BaseVector2Template<double_t>;
 	using Vector2s = IntegralVector2Template<int16_t, float_t>;
 	using Vector2u = IntegralVector2Template<uint32_t, float_t>;
 	using Vector2i = IntegralVector2Template<int32_t, float_t>;
