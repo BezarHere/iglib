ef909ef39709151257f3a5e4740e0073d59b7634
botatodev <botatodev@gmail.com>
2023-12-23 23:27:24 +0300

    changed Colors to Color16

--- iglib/src/_iglib_color.h
+++ iglib/src/_iglib_color.h
@@ -8,33 +8,34 @@ namespace ig
 	{
 		using value_type = _T;
 		using this_type = BaseColorTemplate<_T, _DEFAULT>;
-		static constexpr value_type DefaultValue = value_type(_DEFAULT);
+		static constexpr value_type full_value = value_type(_DEFAULT);
 
 		constexpr BaseColorTemplate()
-			: r{}, g{}, b{}, a{ value_type(DefaultValue) }
+			: r{}, g{}, b{}, a{ value_type(full_value) }
 		{}
 
-		constexpr BaseColorTemplate(value_type rr, value_type gg, value_type bb, value_type aa = value_type(DefaultValue))
+		constexpr BaseColorTemplate(value_type rr, value_type gg, value_type bb, value_type aa = value_type(full_value))
 			: r{ rr }, g{ gg }, b{ bb }, a{ aa }
 		{}
 
 		constexpr this_type inverted(const bool use_alpha = false) const
 		{
-			return { DefaultValue - r, DefaultValue - g, DefaultValue - b, use_alpha ? a : (DefaultValue - a) };
+			return { full_value - r, full_value - g, full_value - b, use_alpha ? a : (full_value - a) };
 		}
 
 		constexpr this_type brighten(const value_type factor, const bool use_alpha = false) const
 		{
-			return { DefaultValue * factor, DefaultValue * factor, DefaultValue * factor, use_alpha ? DefaultValue : (DefaultValue * factor) };
+			return { full_value * factor, full_value * factor, full_value * factor, use_alpha ? full_value : (full_value * factor) };
 		}
 
 		value_type r, g, b, a;
 	};
 
-	
+
 	using Colorb = BaseColorTemplate<byte, 255>;
-	using Colors = BaseColorTemplate<uint16_t, 65535>;
+	using Color16 = BaseColorTemplate<uint16_t, 65535>;
 	using Colorf = BaseColorTemplate<float_t, 1>;
+
 	//using Colorcomplex = BaseColorTemplate<std::complex<std::complex<std::complex<std::complex<std::complex<std::complex<double>>>>>>, -1>;
 
 	//constexpr size_t c = sizeof(Colorcomplex);
