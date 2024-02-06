f369c630e2915f94d93dc2c26153e43699287b5a
botatodev <botatodev@gmail.com>
2024-02-06 23:05:41 +0300

    formating and removed the version from the iglib.cpp (now in iglib.h)

--- iglib/src/iglib.cpp
+++ iglib/src/iglib.cpp
@@ -2,39 +2,31 @@
 #include "iglib.h"
 #include "internal.h"
 
-constexpr Version CurrentVersion{ 1, 7, 0 };
 
 namespace ig
 {
 
-	Error init()
-	{
+	Error init() {
 
-		
+
 		return Error();
-	}
-
-	Version IGlib::version() noexcept
-	{
-		return CurrentVersion;
 	}
 
 	bool IGlib::set_opengl_version( Version v ) {
 		return set_glfw_context_version( v.major, v.minor );
 	}
 
-	
-	std::string get_opengl_version()
-	{
-		auto c = glGetString(GL_VERSION);
+
+	std::string get_opengl_version() {
+		auto c = glGetString( GL_VERSION );
 		// glew not init
 		if (!c)
 		{
-			warn("Create an ig::Window to get the current OpenGL Version\n");
+			warn( "Create an ig::Window to get the current OpenGL Version\n" );
 			return std::string();
 		}
 
-		return std::string((const char *)c);
+		return std::string( (const char *)c );
 	}
 
 }
