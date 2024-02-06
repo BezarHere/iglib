7f537b254d3b811266ecd3cd92c1bc23e9bbf7c7
botatodev <botatodev@gmail.com>
2024-02-06 23:05:09 +0300

    moved version to iglib.h header and made it a static constexpr

--- iglib/src/iglib.h
+++ iglib/src/iglib.h
@@ -15,12 +15,14 @@ namespace ig
 		uint8_t major, minor, patch;
 	};
 
+
+
 	class IGlib
 	{
 	public:
+		static constexpr Version version = { 1, 7, 0 };
 		IGlib() = delete;
 
-		static Version version() noexcept;
 		/// @brief sets the opengl version, versions range from 3.0 to 3.3 and 4.0 to 4.6
 		/// @brief the default version is 4.1 and the patch number is unused
 		/// @return weather the change was successful
